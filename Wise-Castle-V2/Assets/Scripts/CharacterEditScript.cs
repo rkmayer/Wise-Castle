76e607e1f6631530f75eab0596c0174ed8ff42d5
rkmayer <mayerryan98@gmail.com>
2020-03-18 22:58:44 -0400

    now able to edit character

--- Wise-Castle-V2/Assets/Scripts/CharacterEditScript.cs
+++ Wise-Castle-V2/Assets/Scripts/CharacterEditScript.cs
@@ -10,6 +10,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharacterEditScript : MonoBehaviour
 {
@@ -20,18 +21,18 @@ public class CharacterEditScript : MonoBehaviour
 							mouth_1, mouth_2;
 	//character slots
 	public GameObject sprite_body, sprite_eyes, sprite_hair, sprite_mouth;
-	//buttons for changing slots
+	//option buttons
 	public Button body1, body2, body3, body4, body5,
-							eyes1, eyes2, eyes3, eyes4,
-							hair1,
-							mouth1, mouth2;
+						eyes1, eyes2, eyes3, eyes4,
+						hair1,
+						mouth1, mouth2;
 	
     // Start is called before the first frame update
     void Start()
     {
 		//listeners for buttons
 		//body
-        body1.onClick.AddListener(() => updateBody(1));
+		body1.onClick.AddListener(() => updateBody(1));
 		body2.onClick.AddListener(() => updateBody(2));
 		body3.onClick.AddListener(() => updateBody(3));
 		body4.onClick.AddListener(() => updateBody(4));
@@ -50,12 +51,13 @@ public class CharacterEditScript : MonoBehaviour
 		mouth1.onClick.AddListener(() => updateMouth(1));
 		mouth2.onClick.AddListener(() => updateMouth(2));
 		
-		//set slots to saved sprites
-		SpriteRenderer bodyRenderer = sprite_body.GetComponent<SpriteRenderer>();
-		SpriteRenderer eyesRenderer = sprite_eyes.GetComponent<SpriteRenderer>();
-		SpriteRenderer hairRenderer = sprite_hair.GetComponent<SpriteRenderer>();
-		SpriteRenderer mouthRenderer = sprite_mouth.GetComponent<SpriteRenderer>();
-		
+		//update player preview
+		updatePlayer();
+    }
+	
+	//update function
+	void updatePlayer(){
+		//get ids of items in each slot
 		int body_id = PlayerPrefs.GetInt("PlayerBody", 1);
 		int eyes_id = PlayerPrefs.GetInt("PlayerEyes", 1);
 		int hair_id = PlayerPrefs.GetInt("PlayerHair", 1);
@@ -64,71 +66,75 @@ public class CharacterEditScript : MonoBehaviour
 		//body
 		switch(body_id){
 			case(1):
-			bodyRenderer.sprite = body_1;
+			sprite_body.GetComponent<SpriteRenderer>().sprite = body_1;
 			break;
 			case(2):
-			bodyRenderer.sprite = body_2;
+			sprite_body.GetComponent<SpriteRenderer>().sprite = body_2;
 			break;
 			case(3):
-			bodyRenderer.sprite = body_3;
+			sprite_body.GetComponent<SpriteRenderer>().sprite = body_3;
 			break;
 			case(4):
-			bodyRenderer.sprite = body_4;
+			sprite_body.GetComponent<SpriteRenderer>().sprite = body_4;
 			break;
 			case(5):
-			bodyRenderer.sprite = body_5;
+			sprite_body.GetComponent<SpriteRenderer>().sprite = body_5;
 			break;
 		}
 		
 		//eyes
 		switch(PlayerPrefs.GetInt("PlayerEyes", 1)){
 			case(1):
-			eyesRenderer.sprite = eyes_1;
+			sprite_eyes.GetComponent<SpriteRenderer>().sprite = eyes_1;
 			break;
 			case(2):
-			bodyRenderer.sprite = eyes_2;
+			sprite_eyes.GetComponent<SpriteRenderer>().sprite = eyes_2;
 			break;
 			case(3):
-			bodyRenderer.sprite = eyes_3;
+			sprite_eyes.GetComponent<SpriteRenderer>().sprite = eyes_3;
 			break;
 			case(4):
-			bodyRenderer.sprite = eyes_4;
+			sprite_eyes.GetComponent<SpriteRenderer>().sprite = eyes_4;
 			break;
 		}
 		
 		//hair
 		switch(PlayerPrefs.GetInt("PlayerHair", 1)){
 			case(1):
-			hairRenderer.sprite = hair_1;
+			sprite_hair.GetComponent<SpriteRenderer>().sprite = hair_1;
 			break;
 		}
 		
 		//mouth
 		switch(PlayerPrefs.GetInt("PlayerMouth", 1)){
 			case(1):
-			mouthRenderer.sprite = mouth_1;
+			sprite_mouth.GetComponent<SpriteRenderer>().sprite = mouth_1;
 			break;
 			case(2):
-			mouthRenderer.sprite = mouth_2;
+			sprite_mouth.GetComponent<SpriteRenderer>().sprite = mouth_2;
 			break;
 		}
-    }
+	}
 	
 	//update player prefs
 	//body
 	void updateBody(int id){
 		PlayerPrefs.SetInt("PlayerBody", id);
+		updatePlayer();
 	}
 	//eyes
 	void updateEyes(int id){
 		PlayerPrefs.SetInt("PlayerEyes", id);
+		updatePlayer();
 	}
 	//hair
 	void updateHair(int id){
 		PlayerPrefs.SetInt("PlayerHair", id);
+		updatePlayer();
 	}
 	//mouth
 	void updateMouth(int id){
 		PlayerPrefs.SetInt("PlayerMouth", id);
+		updatePlayer();
 	}
 }
